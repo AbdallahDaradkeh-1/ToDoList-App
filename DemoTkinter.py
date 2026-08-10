c28ce21969dac46e8f45295011e9fcc7fdafaca6
AbdallahDaradkeh-1 <daradkehabdallah7@gmail.com>
2026-08-11 00:19:44 +0300

    Link Frame to root and grid it to window, list TasksLists and make a space between name and date using padx, using sticky to stick label to one side inside its cell

--- DemoTkinter.py
+++ DemoTkinter.py
@@ -13,12 +13,26 @@ thisTasksList = operations.tasks_lists
 root = Tk()
 
 root.geometry("800x600")
-tasksListName = Entry(root)
-tasksListName.grid(column=0,row=0)
 
+taskListLabel = Label(root, text = "TaskLists", font='bold')
+taskListLabel.grid(column=0, row=0)
+
+frame = Frame(root, )
+frame.grid(column=1,row=1)
+customRow = 0
 for tasksList in thisTasksList:
-  label = Label(root,text=f"{tasksList.name} {tasksList.date}")
-  label.grid()
+  label = Label(frame,text=f"{tasksList.name}")
+  label.grid(sticky='e', column=0, row=customRow, padx=50)
+
+  customRow += 1
+customRow = 0
+for tasksList in thisTasksList:
+  label = Label(frame,text=f"{tasksList.date}")
+  label.grid(sticky='e', column=1, row=customRow)
+
+  customRow += 1
+
+
 
 
 root.mainloop()
