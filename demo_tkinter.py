64cc595c73508c0ac8c11b3f6e935f83f483c3ea
AbdallahDaradkeh-1 <daradkehabdallah7@gmail.com>
2026-08-18 22:38:08 +0300

    Create Textbox and Button in Tkinter python, when user write a title for the tasks_list and click Add button, this task_list is created and added to local storage and User Interface is updated dynamically through a custom function(refresh_tasks_list()). I created add_tasks_list() method that is called once the Add Button is clicked which take the title and save the tasks_list in local_storage

---
+++ demo_tkinter.py
@@ -0,0 +1,95 @@
+from tkinter import *
+from to_do_list_operations import ToDoListOperations
+
+operations = ToDoListOperations()
+
+this_tasks_list = []
+
+
+
+
+
+root = Tk()
+
+root.geometry("800x600")
+
+taskListLabel = Label(root, text = "TaskLists", font='bold')
+
+
+taskListLabel.grid(column=0, row=0)
+
+create_frame = Frame(root, padx=10, pady=10, background="yellow")
+create_frame.grid()
+
+task_title = Entry(create_frame)
+
+
+def save_task_list():
+  task_title_value = task_title.get()
+  if task_title_value:
+       operations.add_tasks_list(task_title_value)
+       task_title.delete(0, END)
+
+     
+  refresh_tasks_list()
+
+create_task_label = Label(create_frame, text="Add A TaskList Title")
+submit_task_list_button = Button(
+  create_frame,
+  text="Add",
+  command= save_task_list
+)
+
+submit_task_list_button.grid()
+
+
+create_task_label.grid()
+task_title.grid()
+
+
+
+
+tasks_list_frame = Frame(root)
+tasks_list_frame.grid()
+
+custom_row = 0
+
+this_tasks_list = operations.tasks_lists
+
+for tasks_list in this_tasks_list:
+    label = Label(tasks_list_frame,text=f"{tasks_list.name}")
+    label.grid(sticky='e', column=0, row=custom_row, padx=50)
+
+    custom_row += 1
+custom_row = 0
+for tasks_list in this_tasks_list:
+    label = Label(tasks_list_frame,text=f"{tasks_list.date}")
+    label.grid(sticky='e', column=1, row=custom_row)
+
+    custom_row += 1
+
+
+def refresh_tasks_list():
+  for widget in tasks_list_frame.winfo_children():
+    widget.destroy()
+
+  this_tasks_list = operations.tasks_lists
+  custom_row = 0
+
+  for tasks_list in this_tasks_list:
+    label = Label(tasks_list_frame,text=f"{tasks_list.name}")
+    label.grid(sticky='e', column=0, row=custom_row, padx=50)
+
+    custom_row += 1
+  custom_row = 0
+  for tasks_list in this_tasks_list:
+    label = Label(tasks_list_frame,text=f"{tasks_list.date}")
+    label.grid(sticky='e', column=1, row=custom_row)
+
+    custom_row += 1
+
+
+
+
+
+root.mainloop()
