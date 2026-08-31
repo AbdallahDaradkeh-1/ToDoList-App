10b5c24dac3c56edcc1b49d9a90944411af6ef7b
AbdallahDaradkeh-1 <daradkehabdallah7@gmail.com>
2026-08-31 09:57:20 +0300

    Add Scrollbar to window through Canvas Widget because scrollbar cannot be added directly to frame. The problem that: I should play around a little bet with scrollbar setup to become cofident. And another problem is I should organize and polish the code

--- tkinter_gui/demo_tkinter_dynamic.py
+++ tkinter_gui/demo_tkinter_dynamic.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import tkinter as tk
 from to_do_list_operations import ToDoListOperations
 from tkinter import ttk
 from .tkinter_functions import screen_configurations 
@@ -10,19 +11,38 @@ from .tkinter_tasks_list_operations import create_tasks_list_form
 operations = ToDoListOperations()
 root = screen_configurations()
 
-tasks_list_header_label = ttk.Label(root, text = "TaskLists", font='bold')
-tasks_list_header_label.grid(column=0, row=0)
+canvas = tk.Canvas(root, width= 700, height=600)
+canvas.pack(side='left', fill='both', expand='True')
+scrollbar = tk.Scrollbar(root, orient='vertical', command=canvas.yview)
+scrollbar.pack(side='right', fill='y')
+canvas.configure(yscrollcommand=scrollbar.set)
+# tasks_list_header_label = ttk.Label(root, text = "TaskLists", font='bold')
+# tasks_list_header_label.pack()
 
-create_frame = Frame(root, padx=20, pady=20, background="grey")
-create_frame.grid()
+content_frame = Frame(canvas)
+content_frame.pack()
 
-tasks_list_frame = Frame(root)
-tasks_list_frame.grid()
+
+create_frame = Frame(content_frame, padx=20, pady=20, background="grey")
+create_frame.pack(fill='x')
+canvas.create_window((0,0), window=content_frame, anchor='nw')
+content_frame.bind(
+    "<Configure>",
+    lambda event: canvas.configure(scrollregion=canvas.bbox("all"))
+)
+tasks_list_frame = Frame(content_frame)
+tasks_list_frame.pack()
+
+tasks_list_frame.bind(
+    "<Configure>",
+    lambda e: canvas.configure(scrollregion=canvas.bbox("all"))
+)
 
 create_tasks_list_form(create_frame, operations, tasks_list_frame)
 
-
+# print(tasks_list_header_label.keys)
 tasks_lists_form(tasks_list_frame, operations)
-
+# for option in create_frame.keys():
+#     print(f"{option}: {create_frame.cget(option)}")
 
 root.mainloop()
