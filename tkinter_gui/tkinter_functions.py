af1a32fad59e8acd468945bce61105e0531fbc69
AbdallahDaradkeh-1 <daradkehabdallah7@gmail.com>
2026-09-03 13:35:42 +0300

    Delete Feature: Add delete button that call delete_task_list() function once clicked, create delete_task_list() function that delete tasks_list through delete_task_list() function. and refersh the task list to update the UI. Move many functions from tkinter_tasks_list_operations.py module to tkinter_functions.py module due to difficulty in managing import(circullar import error)

--- tkinter_gui/tkinter_functions.py
+++ tkinter_gui/tkinter_functions.py
@@ -24,6 +24,13 @@ def tasks_lists_form(frame, operations_object):
       edit_action.config(command=lambda new_entry = entry, edit_action = edit_action, the_frame = frame, action_row = custom_row, operations_object =operations_object, tasks_list_id = tasks_list_id : edit_task_list(new_entry,edit_action, the_frame, action_row, operations_object, tasks_list_id)
             )
       edit_action.grid( row=custom_row, sticky='e')
+      delete_action = ttk.Button(
+          frame,
+          text="Delete"
+      )
+      delete_action.grid(row=custom_row, sticky='e', column= 4)
+      delete_action.config(command= lambda tasks_list_id = tasks_list_id, the_frame = frame : delete_task_list(tasks_list_id, the_frame, operations_object))
+      
 
       custom_row += 1
   custom_row = 0
@@ -49,6 +56,10 @@ def edit_task_list(new_entry, edit_action, the_frame, action_row, operations_obj
 
     save_button.config(command= lambda entry = new_entry, save_button = save_button, cancel_button = cancel_button, operations_object= operations_object, edit_action = edit_action, tasks_list_id = tasks_list_id : save_new_task_list(entry, save_button, cancel_button, operations_object, edit_action, tasks_list_id))
 
+def delete_task_list(tasks_list_id, the_frame, operations_object):
+    operations_object.delete_task_list(tasks_list_id)
+    refresh_tasks_list(operations_object, the_frame)
+    
 
 
 def save_new_task_list(entry, save_button, cancel_button, operations_object, edit_action, tasks_list_id):  
@@ -61,4 +72,67 @@ def save_new_task_list(entry, save_button, cancel_button, operations_object, edi
     entry.config(state='readonly')
     
 
+
+
+def create_tasks_list_form(frame, operations_object, tasks_list_frame):
+    
+  create_label = ttk.Label(frame, text="Create A TaskList Title")
+  tasks_list_subject = ttk.Entry(frame)
+  create_button = ttk.Button(
+    frame,
+    text="Create",
+    command= lambda: save_task_list(tasks_list_subject, operations_object, tasks_list_frame)
+  )
+
+
+  create_label.grid(padx=5, pady=5)
+  tasks_list_subject.grid(padx=5)
+  create_button.grid(row=1,column=1)
+
+def save_task_list(subject, operations_object, frame):
+  task_title_value = subject.get()
+  if task_title_value:
+       operations_object.add_tasks_list(task_title_value)
+       subject.delete(0, END)
+
+     
+  refresh_tasks_list(operations_object, frame)
+
+
+def refresh_tasks_list(operations_object, frame):
+  for widget in frame.winfo_children():
+    widget.destroy()
+
+  these_tasks_lists = operations_object.tasks_lists
+  custom_row = 0
+
+  for tasks_list in these_tasks_lists:
+                entry = ttk.Entry(frame)
+                entry.insert(0, tasks_list.name)
+                entry.config(state="readonly")
+                entry.grid(sticky='e', column=0, row=custom_row, padx=50)
+                edit_action = ttk.Button(
+                  frame,
+                  text="Edit")
+                tasks_list_id = tasks_list.id
+                edit_action.config(command=lambda new_entry = entry, edit_action = edit_action, the_frame = frame, action_row = custom_row, operations_object =operations_object, tasks_list_id = tasks_list_id : edit_task_list(new_entry,edit_action, the_frame, action_row, operations_object, tasks_list_id)
+                      )
+                edit_action.grid( row=custom_row, sticky='e')
+                delete_action = ttk.Button(
+                    frame,
+                    text="Delete"
+                )
+                delete_action.grid(row=custom_row, sticky='e', column= 4)
+                delete_action.config(command= lambda tasks_list_id = tasks_list_id, the_frame = frame : delete_task_list(tasks_list_id, the_frame, operations_object))
+    
+                custom_row += 1
+    
+  custom_row = 0
+  for tasks_list in these_tasks_lists:
+    entry = Entry(frame,)
+    entry.insert(0, tasks_list.date)
+    entry.config(state="readonly")
+    entry.grid(sticky='e', column=1, row=custom_row)
+
+    custom_row += 1 
 
